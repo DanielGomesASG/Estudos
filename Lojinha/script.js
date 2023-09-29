6ee521057acde94bd973e6620328736bdfa80c5f
Daniel Gomes <danielgomesasg@gmail.com>
2023-09-28 22:04:15 -0300

    Pequenas modificações

--- Lojinha/script.js
+++ Lojinha/script.js
@@ -1,5 +1,4 @@
 let user = new Object()
-user.nome = "Daniel"
 user.b = 0
 user.cheio = () => {
   if (user.b >= 10) {
@@ -13,7 +12,7 @@ pao.t = 12
 let carne = new Object()
 carne.t = 15
 
-let comidaOriginal = prompt("Olá, em nossa loja você encontrará MUITA variedade, MUITA mesmo, pelo que eu vejo sua barriga precisa de 10 nutrientes para ficar cheia, dê uma olhada no cardápio!\n\nCarne: 15 mordidas, 2 nutrientes para cada\nPão: 12 mordidas, 1 nutriente para cada\n\nPode escolher, demore o quanto precisar XD!")
+let comidaOriginal = prompt("Olá, em nossa loja você encontrará MUITA variedade, MUITA mesmo, pelo que eu vejo sua barriga precisa de 10 nutrientes para ficar cheia, dê uma olhada no cardápio!\n\nCarne: 15 mordidas, 2 nutrientes para cada\nPão: 12 mordidas, 1 nutriente para cada\n\nPode escolher, demore o quanto precisar!")
 
 function removerAcentos(comidaOriginal) {
   return comidaOriginal.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
@@ -39,7 +38,7 @@ while (!user.cheio()) {
 }
 
 if (comida === "pao") {
-  alert("Seu buchinho agora está cheio!\nSeu pãozinho só tem mais " + pao.t + " mordidinhas, já embalamos para viagem, assim vc pode se deliciar depois.\n\nMuito obrigado pela visita! Volte Sempre!!! XD")
+  alert("Sua barriga agora está cheia!\nSeu pão só tem mais " + pao.t + " mordidas, já embalamos para viagem, assim vc pode se deliciar depois.\n\nMuito obrigado pela visita! Volte Sempre!!!")
 } else if (comida === "carne") {
-  alert("Seu buchinho agora está cheio!\nSua carninha só tem mais " + carne.t + " mordidinhas, já embalamos para viagem, assim vc pode se deliciar depois.\n\nMuito obrigado pela visita! Volte Sempre!!! XD ")
+  alert("Sua barriga agora está cheia!\nSua carne só tem mais " + carne.t + " mordidas, já embalamos para viagem, assim vc pode se deliciar depois.\n\nMuito obrigado pela visita! Volte Sempre!!!")
 }
